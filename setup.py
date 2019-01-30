9b9177b6e1d524a8f005f7f54871acfc90db5916
Mr. Senko <atodorov@mrsenko.com>
2019-01-30 03:37:26 +0200

    Version 0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIREMENTS = open('requirements.txt').readlines()
 
 
 setup(name='kiwitcms-junit.xml-plugin',
-      version='0.0',
+      version='0.2',
       packages=['tcms_junit_plugin'],
       scripts=['tcms-junit.xml-plugin'],
       description='junit.xml plugin for '
