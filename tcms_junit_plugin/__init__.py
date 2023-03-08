c2b3e9abd24b5357b735d4d54674ad439ad98da0
Michael Kaye <1917473+michaelkaye@users.noreply.github.com>
2023-03-08 17:42:29 +0000

    Allow conversion from xml testcase to summary to be templated via commandline.

--- tcms_junit_plugin/__init__.py
+++ tcms_junit_plugin/__init__.py
@@ -4,6 +4,7 @@
 import argparse
 
 from junitparser import Error, Failure, JUnitXml, Skipped
+from string import Template
 from tcms_api import plugin_helpers
 
 from .version import __version__
@@ -15,8 +16,9 @@ class Backend(plugin_helpers.Backend):
 
 
 class Plugin:  # pylint: disable=too-few-public-methods
-    def __init__(self, verbose=False):
+    def __init__(self, summary_template, verbose=False):
         self.backend = Backend(prefix='[junit.xml]', verbose=verbose)
+        self.summary_template = summary_template
         self.verbose = verbose
 
     def parse(
@@ -42,8 +44,13 @@ class Plugin:  # pylint: disable=too-few-public-methods
             else:
                 cases = list(xml)
 
+            summary_template = Template(self.summary_template)
             for xml_case in cases:
-                summary = f"{xml_case.classname}.{xml_case.name}"[:255]
+                # Only permit non-secret values in this map
+                # Users with template control can retrieve any value set here.
+                values = {"classname": xml_case.classname, "name": xml_case.name}
+
+                summary = summary_template.substitute(values)[:255]
 
                 test_case, _ = self.backend.test_case_get_or_create(summary)
                 self.backend.add_test_case_to_plan(test_case['id'],
@@ -93,10 +100,13 @@ def main(argv):
     )
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
                         help='Print information about created TP/TR records')
+    parser.add_argument('--summary-template', dest='summary_template', type=str,
+                        help='Template to convert testcase to summary, eg %(default)s.',
+                        default='${classname}.${name}')
     parser.add_argument('filename.xml', type=str, nargs='+',
                         help='XML file(s) to parse')
 
     args = parser.parse_args(argv[1:])
 
-    plugin = Plugin(verbose=args.verbose)
+    plugin = Plugin(args.summary_template, verbose=args.verbose)
     plugin.parse(getattr(args, 'filename.xml'))
